7fb6024906ed01c726619986ea7df8f4f284397c
lmknai-jd <104866991+lmknai-jd@users.noreply.github.com>
2022-06-17 17:21:13 -0400

    Add files via upload

---
+++ datatablethingidk.py
@@ -0,0 +1,9 @@
+import numpy as np
+import natplotlib.pyplot as plt
+import random
+
+x=2 * np.random.rand(100,1)
+y=4+3 * X+np.random.rand(100,1)
+
+from sklearn.lenear_model import LinearRegression
+
